e3f2705d9be856d0cdafed794985e1449c3558ba
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2025-05-03 22:35:48 +0700

    chore: example mux (having)

---
+++ mux-having/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	datatables "github.com/ZihxS/golang-gorm-datatables" // [👈🏼 FOCUS HERE]
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	ID   int
+	Name string
+	Age  int
+}
+
+func main() {
+	dsn := "..." // [👈🏼 ADJUST HERE]
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database instance:", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/having", func(w http.ResponseWriter, r *http.Request) {
+		req, err := datatables.ParseRequest(r) // [👈🏼 FOCUS HERE]
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing request: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		// [👇🏼 FOCUS HERE]
+		tx := db.
+			Select("SELECT users.id, users.name, users.age, COUNT(orders.id) AS total_orders").
+			Table("users").
+			Joins("LEFT JOIN orders ON users.id = orders.user_id").
+			Group("users.id, users.name, users.age").
+			Having("total_orders >= 1")
+		// [👆🏻 FOCUS HERE]
+
+		// [👇🏼 FOCUS HERE]
+		datatable := datatables.New(tx).Req(*req)
+		datatable.WithNumber()
+		// [👆🏻 FOCUS HERE]
+
+		response, err := datatable.Make() // [👈🏼 FOCUS HERE]
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing datatables: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(response)
+	})
+
+	log.Fatal(http.ListenAndServe(":6969", r))
+}
