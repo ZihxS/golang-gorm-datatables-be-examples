98dca867ae5760d6372c771fa9dc0c889e6d8512
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2025-05-04 21:26:28 +0700

    chore: example mux (distinct)

---
+++ mux-distinct/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	datatables "github.com/ZihxS/golang-gorm-datatables" // [👈🏼 FOCUS HERE]
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Order struct {
+	ID          int
+	UserID      int
+	ProductName string
+	Quantity    int
+	Price       float64
+	OrderDate   *time.Time
+}
+
+func main() {
+	dsn := "..." // [👈🏼 ADJUST HERE]
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database instance:", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/distinct", func(w http.ResponseWriter, r *http.Request) {
+		req, err := datatables.ParseRequest(r) // [👈🏼 FOCUS HERE]
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing request: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		// [👇🏼 FOCUS HERE]
+		tx := db.
+			Model(&Order{}).
+			Distinct("product_name")
+
+		response, err := datatables.
+			New(tx).
+			Req(*req).
+			WithNumber().
+			Make()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing request: %v", err), http.StatusInternalServerError)
+			return
+		}
+		// [👆🏼 FOCUS HERE]
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(response)
+	})
+
+	log.Fatal(http.ListenAndServe(":6969", r))
+}
