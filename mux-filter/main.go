cdc28daebce5c4caac211b7f902b1d7a8622f4bd
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2025-05-04 21:26:33 +0700

    chore: example mux (filter)

---
+++ mux-filter/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	datatables "github.com/ZihxS/golang-gorm-datatables" // [👈🏼 FOCUS HERE]
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func main() {
+	dsn := "..." // [👈🏼 ADJUST HERE]
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database instance:", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/filter", func(w http.ResponseWriter, r *http.Request) {
+		req, err := datatables.ParseRequest(r)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing request: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		tx := db.
+			Select("SELECT o.product_name, o.price, o.quantity, (o.quantity*o.price) total_price, u.name buyer, p.details buyer_profile_detail, t.tag_name").
+			Table("orders o").
+			Joins("INNER JOIN users u on u.id = o.user_id").
+			Joins("INNER JOIN profiles p on p.user_id = u.id").
+			Joins("LEFT JOIN user_tags ut on ut.user_id = u.id").
+			Joins("LEFT JOIN tags t on t.id = ut.tag_id")
+
+		// [👇🏼 FOCUS HERE]
+		datatable := datatables.New(tx).Req(*req)
+		datatable.WithNumber()
+		// [👆🏼 FOCUS HERE]
+
+		// [👇🏼 FOCUS HERE]
+		datatable.
+			Filter(func(db *gorm.DB) *gorm.DB {
+				return db.Where("o.quantity > 1")
+			}).
+			Filter(func(db *gorm.DB) *gorm.DB {
+				return db.Where("o.price*o.quantity > 150")
+			})
+		// [👆🏼 FOCUS HERE]
+
+		// [👇🏼 FOCUS HERE]
+		response, err := datatable.Make()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing request: %v", err), http.StatusInternalServerError)
+			return
+		}
+		// [👆🏼 FOCUS HERE]
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(response)
+	})
+
+	log.Fatal(http.ListenAndServe(":6969", r))
+}
